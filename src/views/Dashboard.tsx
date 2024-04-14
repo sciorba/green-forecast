76d181a98500df02b2850527fd370c50bb636ffe
sciorba <stefan.c.0608@gmail.com>
2024-04-14 10:07:23 +0300

    optimize

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,76 +1,14 @@
 import Grid from '@mui/material/Grid';
 import {CharacteristicCard} from "../components/CharacteristicCard.tsx";
-import HomeTwoToneIcon from '@mui/icons-material/HomeTwoTone';
 import {OverridableComponent} from "@mui/material/OverridableComponent";
-import OfflineBoltTwoToneIcon from '@mui/icons-material/OfflineBoltTwoTone';
-import ChaletTwoToneIcon from '@mui/icons-material/ChaletTwoTone';
-import BuildCircleTwoToneIcon from '@mui/icons-material/BuildCircleTwoTone';
-import PublicTwoToneIcon from '@mui/icons-material/PublicTwoTone';
-import SolarPowerTwoToneIcon from '@mui/icons-material/SolarPowerTwoTone';
-import WorkspacePremiumTwoToneIcon from '@mui/icons-material/WorkspacePremiumTwoTone';
-import FloodTwoToneIcon from '@mui/icons-material/FloodTwoTone';
-import BathroomTwoToneIcon from '@mui/icons-material/BathroomTwoTone';
-import SpaTwoToneIcon from '@mui/icons-material/SpaTwoTone';
 import {SvgIconTypeMap} from "@mui/material";
+import {characteristics} from "../constants.tsx";
 
 export interface Characteristic {
   Icon: OverridableComponent<SvgIconTypeMap> & { muiName: string };
   title: string;
   characteristic: string | null;
 }
-
-const characteristics: Characteristic[] = [
-  {
-    Icon: HomeTwoToneIcon,
-    title: 'Housing quality',
-    characteristic: 'Serious defects'
-  },
-  {
-    Icon: OfflineBoltTwoToneIcon,
-    title: 'Energy',
-    characteristic: 'Enough'
-  },
-  {
-    Icon: ChaletTwoToneIcon,
-    title: 'Insulation',
-    characteristic: 'Mediocre'
-  },
-  {
-    Icon: BuildCircleTwoToneIcon,
-    title: 'Installations',
-    characteristic: 'All inspected'
-  },
-  {
-    Icon: PublicTwoToneIcon,
-    title: 'Soil',
-    characteristic: 'Included in register'
-  },
-  {
-    Icon: SolarPowerTwoToneIcon,
-    title: 'Solar potential',
-    characteristic: 'Usable'
-  },
-  {
-    Icon: WorkspacePremiumTwoToneIcon,
-    title: 'Permits',
-    characteristic: null
-  },
-  {
-    Icon: FloodTwoToneIcon,
-    title: 'Flood risk',
-    characteristic: 'Small chance'
-  },
-  {
-    Icon: BathroomTwoToneIcon,
-    title: 'Water and sewerage',
-    characteristic: 'Sewerage available'
-  },
-  {
-    Icon: SpaTwoToneIcon,
-    title: 'Teal level',
-    characteristic: 'Not known'
-  },
-]
 
 export const Dashboard = () => {
   return (
