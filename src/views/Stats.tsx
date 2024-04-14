c7c74f94f92704c23c14106856bf25ec2334f2ff
sciorba <stefan.c.0608@gmail.com>
2024-04-14 17:04:49 +0300

    add image

--- src/views/Stats.tsx
+++ src/views/Stats.tsx
@@ -1,16 +1,8 @@
-import { DatePicker } from "@mui/x-date-pickers";
-import dayjs from "dayjs";
-import LineDataset from "../components/Chart.tsx";
+import image from '../img.png'
 
 const Stats = () => {
   return (
-    <div style={{ marginTop: 80 }}>
-      <div style={{ display: 'flex', gap: '4rem', marginLeft: '4rem' }}>
-          <DatePicker label="Uncontrolled picker" defaultValue={dayjs('2024-03-10')} />
-          <DatePicker label="Uncontrolled picker" defaultValue={dayjs('2042-04-10')} />
-      </div>
-      <LineDataset/>
-    </div>
+    <img src={image} alt="image" />
   );
 };
 
