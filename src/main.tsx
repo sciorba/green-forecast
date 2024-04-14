b81f690f2d43cbc1f29bb5748787a9f2cfc17b8c
sciorba <stefan.c.0608@gmail.com>
2024-04-14 10:45:35 +0300

    change routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {routes} from "./routes.tsx";
+import {BrowserRouter} from "react-router-dom";
+import {AppRoutes} from "./routes.tsx";
+import ResponsiveDrawer from "./components/Drawer.tsx";
 
-const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <BrowserRouter>
+      <>
+        <ResponsiveDrawer />
+        <AppRoutes/>
+      </>
+    </BrowserRouter>
   </React.StrictMode>,
 )
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,13 @@
-import {RouteObject} from "react-router/dist/lib/context";
 import {Dashboard} from "./views/Dashboard.tsx";
+import {Route, Routes} from "react-router-dom";
 
-export const routes: RouteObject[] = [
-  {
-    path: "/",
-    element: <Dashboard/>,
-  },
-  {
-    path: '/form',
-    element: <div>form</div>
-  }
-]
+export const AppRoutes = () => {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard/>}>
+        <Route path="about" element={<div>about</div>} />
+      </Route>
+    </Routes>
+  );
+}
