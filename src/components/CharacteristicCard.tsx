1dc263a8d5d0c637bc308cfabc5b378f9764486a
sciorba <stefan.c.0608@gmail.com>
2024-04-14 10:05:05 +0300

    add check icon

--- src/components/CharacteristicCard.tsx
+++ src/components/CharacteristicCard.tsx
@@ -3,14 +3,18 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import {Characteristic} from "../views/Dashboard.tsx";
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import {Badge} from "@mui/material";
 
 interface CharacteristicCardProps extends Characteristic {}
 
 export const CharacteristicCard = ({ title, characteristic, Icon }: CharacteristicCardProps) => {
 
   return (
-    <Card sx={{ display: 'flex', height: 150 }}>
-      <Icon sx={{ alignSelf: 'center', marginLeft: 2, fontSize: 48}} />
+    <Card sx={{ display: 'flex', height: 150, alignItems: 'center' }}>
+      <Badge badgeContent={<CheckCircleOutlineIcon sx={{ position: 'absolute' }}/>} color="success" >
+        <Icon sx={{ alignSelf: 'center', marginLeft: 2, fontSize: 48}} />
+      </Badge>
       <Box sx={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
         <CardContent sx={{ flex: '1 0 auto' }}>
           <Typography component="div" variant="h5">
